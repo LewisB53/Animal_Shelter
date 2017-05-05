11e8b56c9e825fc256df2fa4409aa0be323e0113
Lewis <lewisburns630@googlemail.com>
2017-05-05 18:18:01 +0100

    test passed, can retrieve pet name from hash

--- specs/animal_spec.rb
+++ specs/animal_spec.rb
@@ -5,7 +5,12 @@ require_relative("../models/animal")
 class TestAnimal < Minitest::Test
 
   def setup
-    @animal1 = Animal.new("hash_goes_here")
+    @animal1 = Animal.new({
+  'name' => "Skip",
+  'age' => 6,
+  'admission_date'=> "11/01/2017",
+  'adoptable' => true
+  })
   end
 
 
@@ -13,5 +18,8 @@ class TestAnimal < Minitest::Test
     assert_equal(Animal, @animal1.class)
   end
 
+ def test_can_return_hash
+    assert_equal("Skip", @animal1.name)
+ end
 
 end
