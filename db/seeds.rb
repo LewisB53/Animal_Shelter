1ae429129364ce75b9e34c3e81028ab414f7ce9e
Lewis <lewisburns630@googlemail.com>
2017-05-06 14:25:17 +0100

    Add pry-byebug to seeds.rb for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 require_relative("../models/animal.rb")
 require_relative("../models/owner.rb")
+require('pry-byebug')
+require_relative('./sqlrunner.rb')
 
 animal1 = Animal.new({
   'name' => "Skip",
@@ -7,3 +9,6 @@ animal1 = Animal.new({
   'admission_date'=> "11/01/2017",
   'adoptable' => true
   })
+
+binding.pry
+nil
