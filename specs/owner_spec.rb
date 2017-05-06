a8877e7aa865d1de151190244f044148ff6ea764
Lewis <lewisburns630@googlemail.com>
2017-05-06 14:01:21 +0100

    Add test can create owner

--- specs/owner_spec.rb
+++ specs/owner_spec.rb
@@ -2,11 +2,16 @@ require ("minitest/autorun")
 require("minitest/rg")
 require_relative("../models/owner")
 
-class TestAnimal < Minitest::Test
+class TestOwner < Minitest::Test
 
   def setup
     @owner1 = Owner.new({"name" => "Lewis"})
   end
+
+  def test_create_owner
+    assert_equal(Owner, @owner1.class)
+  end
+
 
 
 end
