46755df5ac6d3a5b5f67719d582243694fbcd3fd
Lewis <lewisburns630@googlemail.com>
2017-05-07 11:39:34 +0100

    Add Layout.erb with all animals link

--- shelter_controller.rb
+++ shelter_controller.rb
@@ -1,7 +1,7 @@
 require('sinatra')
 require('sinatra/contrib/all')
 require('pry-byebug')
-require_relative('./models/student.rb')
+require_relative('./models/animal.rb')
 require_relative('./db/sqlrunner.rb')
 
 get '/home' do
