215c3317fd3d0fbd6e319c229733d3befdf83f9e
Lewis <lewisburns630@googlemail.com>
2017-05-08 10:54:37 +0100

    add owned method to animal to check specifically owned pets

--- models/animal.rb
+++ models/animal.rb
@@ -3,19 +3,20 @@ class Animal
   attr_accessor :name, :age, :admission_date, :adoptable, :owner_id
 
   def initialize(params)
-    @id = params['id'].to_i
+    @id = params['id'].to_i if params['owner_id'].to_i
     @name = params['name']
     @age = params['age'].to_i
-    @admission_date = params['admission_date']
+    @admission_date = params['admission_date'].to_s
+    # @adoptable_string = params['adoptable'].to_s
     @adoptable = params['adoptable']
-    @owner_id = params['owner_id'].to_i if params['owner_id']
+    @owner_id = params['owner_id'].to_i if params['owner_id'].to_i
   end
 
   def save()
     sql = "INSERT INTO animals 
     (name, age, admission_date, adoptable, owner_id)
     VALUES 
-    ('#{@name}', '#{@age}','#{@admission_date}', '#{@adoptable}', '#{@owner_id}')
+    ('#{@name}', '#{@age}','#{@admission_date}', #{@adoptable}, '#{@owner_id}')
     RETURNING id;"
     saved_animal = SqlRunner.run(sql)
     @id = saved_animal.first()['id'].to_i
@@ -42,12 +43,26 @@ class Animal
    '#{@name}',
    '#{@age}',
    '#{@admission_date}',
-   '#{@adoptable}',
+   #{@adoptable},
    #{@owner_id} 
    )
    WHERE id = #{@id};"
    updated = SqlRunner.run(sql)
    return updated
+ end
+
+ def self.owned()
+   sql = "SELECT * FROM animals WHERE owner_id > 0"
+   users = SqlRunner.run(sql)
+   result = self.map_items(sql)
+   return result
+ end
+
+
+ def self.map_items(sql)
+   animals = SqlRunner.run(sql)
+   result = animals.map { |an_animal| Animal.new( an_animal ) }
+   return result
  end
 
 end
